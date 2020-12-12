6769cc77c6b4d5186a31bf9963a21e187b128f78
MSang <msang77335@gmail.com>
2020-12-12 16:54:18 +0700

    responsive-update

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const footer = document.querySelector(".footer");
 const content = document.querySelector(".content");
 const body = document.querySelector("body");
 const bars = document.querySelector(".bars");
+const btnScrollTop = document.querySelector(".to-top");
 
 window.addEventListener("resize", changeFixedElementWidth);
 window.addEventListener("load", changeFixedElementWidth);
@@ -34,8 +35,19 @@ const btnCloseVideo = document.querySelector(".trailer__close");
 const trailer = document.querySelector(".trailer");
 const trailerVideo = document.querySelector(".trailer__video");
 
+function hiddenEl(el) {
+   el.style.opacity = "0";
+   el.style.visibility = "hidden";
+}
+
+function showEl(el) {
+   el.style.opacity = "1";
+   el.style.visibility = "visible";
+}
+
 btnPlay.addEventListener("click", function () {
-   header.style.zIndex = 10;
+   hiddenEl(header);
+   btnScrollTop.style.display = "none";
    trailer.classList.add("show");
    trailerVideo.play();
 });
@@ -44,5 +56,36 @@ btnCloseVideo.addEventListener("click", function () {
    trailer.classList.remove("show");
    trailerVideo.pause();
    trailerVideo.currentTime = 0;
-   header.style.zIndex = 15;
+   showEl(header);
+   btnScrollTop.style.display = "block";
 });
+
+//Active Button Scroll Top
+btnScrollTop.addEventListener("click", function () {
+   smoothScroll(1000);
+});
+//function smoothScroll(target, duration)
+function smoothScroll(duration) {
+   //var target = document.querySelector(target);
+   //const targetPosition = target.getBoundingClientRect().top;
+   const targetPosition = 0;
+   let startPosition = window.pageYOffset;
+   let distance = targetPosition - startPosition;
+   var startTime = null;
+
+   function animation(currentTime) {
+      if (startTime === null) startTime = currentTime;
+      let timeElapsed = currentTime - startTime;
+      let run = ease(timeElapsed, startPosition, distance, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+   }
+
+   function ease(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+   }
+   requestAnimationFrame(animation);
+}
